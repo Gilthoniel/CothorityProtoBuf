1972fb8d49da6c03a2e358d29208ddd00f0d9859
Gaylor Bosson <gaylor.bosson@gmail.com>
2017-02-27 14:03:52 +0100

    update(): remove console

--- index.test.js
+++ index.test.js
@@ -14,10 +14,9 @@ describe('Protobuf', () => {
             }
           }
         });
-        console.log(encoded);
+
         const decoded = CothorityProtobuf.decodeMessage('StatusResponse', encoded);
 
-        console.log(decoded);
         expect(decoded.system.status1.field.field1).toBe('success');
       })
       .catch((e) => console.log(e));
