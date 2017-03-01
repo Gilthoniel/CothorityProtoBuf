b2a3c831d4464e0e03350906a587b10ba60bb7b8
Gaylor Bosson <gaylor.bosson@gmail.com>
2017-03-01 11:41:27 +0100

    feat(signature): add signature models

--- index.test.js
+++ index.test.js
@@ -10,6 +10,12 @@ const mockStatusResponses = [{
   public: "588addc1fbd9c403868e0d0ac2003e7c7538d0a9154fbdb3cf772822f0ebc827"
 }];
 
+const mockSignatureRequest = [{
+  base64: "CiCAHxMpFWVDC2nQwYfKiXWtrOGwZRS0Aw9T/ZOkwK3ZwxLbBBJeCiDl4j5YU5oJ0yEdj6D7NHXUhlXgwG2D6TyObn0WqofBBhIQsgjTBrNHUfy7s9TpyGWI2RoXdGNwOi8vNzguNDYuMjI3LjYwOjc3NzAiD0luZWl0aSdzIHNlcnZlchJgCiADa/MW4epufpngu3E0GdFsC2eUv53EQsxM82w/k16TzxIQTHNdO8NjVVacTB09sXZd2hoXdGNwOi8vMTkyLjMzLjIxMC44Ojc3NzAiEUVQRkwvREVESVMgQ29ub2RlEm0KIJtU/fuzkTjwpFoI4XIatOslfg8kbJOichVNZRRUdWKQEhBmIqzF2XJbV4rFG+PINEI9Ghl0Y3A6Ly8xODUuMjYuMTU2LjQwOjYxMTE2IhxJc21haWwncyBjb25vZGUgKEB1YmVyc3BhY2UpEmsKIFLGp3x1bOXzvvNBQAbEVVautwhMDz5EZ90nEHknkSxREhAngVp8695Tmb7jB/s9xVgpGhd0Y3A6Ly81LjEzNS4xNjEuOTE6MjAwMCIcTmlra29sYXNnJ3Mgc2VydmVyIGNvdGhvcml0eRJXCiCC6GTQYjDaoukKGsJOxzi82eVFhnfnXx5kqbUUpxHWMBIQXO27l7XxWpuQkfy390DhRhoXdGNwOi8vODMuMjEyLjgyLjIzOjY3ODkiCExlZnRlcmlzEmIKIFiK3cH72cQDho4NCsIAPnx1ONCpFU+9s893KCLw68gnEhC+0DnGfqtY7Y3/oTc1XcN+Ghp0Y3A6Ly85NS4xNDMuMTcyLjI0MTo2MjMwNiIQRGFlaW5hcidzIENvbm9kZQ==",
+  roster: 6,
+  message: '801f13291565430b69d0c187ca8975adace1b06514b4030f53fd93a4c0add9c3'
+}];
+
 describe('Protobuf', () => {
 
   it('should encode and decode correctly', () => {
@@ -33,7 +39,9 @@ describe('Protobuf', () => {
   });
 
   it('should decode a status response correctly', () => {
-    CothorityProtobuf.wait().then(() => {
+    expect.assertions(mockStatusResponses.length * 5);
+    
+    return CothorityProtobuf.wait().then(() => {
       mockStatusResponses.forEach((mock) => {
         const buffer = Uint8Array.from(atob(mock.base64), c => c.charCodeAt(0));
 
@@ -45,6 +53,20 @@ describe('Protobuf', () => {
 
         const pub = CothorityProtobuf.bufferToHex(response.server.public);
         expect(pub).toBe(mock.public);
+      });
+    });
+  });
+  
+  it('should decode a signature request correctly', () => {
+    expect.assertions(2);
+    
+    return CothorityProtobuf.wait().then(() => {
+      mockSignatureRequest.forEach((mock) => {
+        const buffer = Uint8Array.from(atob(mock.base64), c => c.charCodeAt(0));
+        const response = CothorityProtobuf.decodeMessage('SignatureRequest', buffer);
+        
+        expect(response.roster.list.length).toBe(mock.roster);
+        expect(CothorityProtobuf.bufferToHex(response.message)).toBe(mock.message);
       });
     });
   });
