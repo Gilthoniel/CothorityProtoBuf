acfc5e21a9de93656eea8b3b9b892c51bcdda361
Lucio <lucio.romerio@epfl.ch>
2017-04-23 12:08:37 +0200

    Fix typo in ProposeSend

--- src/models/cisc/config.js
+++ src/models/cisc/config.js
@@ -2,9 +2,9 @@ import protobuf from 'protobufjs'
 const {Type, Field, MapField} = protobuf;
 
 const config = new Type("Config")
-    .add(new Field('Threshold', 1, 'sint32'))
-    .add(new MapField('Device', 2, 'string', 'Device'))
-    .add(new MapField('Data', 3, 'string', 'string'));
+    .add(new Field('threshold', 1, 'sint32'))
+    .add(new MapField('device', 2, 'string', 'Device'))
+    .add(new MapField('data', 3, 'string', 'string'));
 
 
 export default config;
